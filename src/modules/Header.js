bb9985fcc4c3b05cd2d9ef7e29a46fb79a132ae5
tech-hunter <kingkushwaha93@gmail.com>
2023-10-28 19:09:31 +0530

    navbar

---
+++ src/modules/Header.js
@@ -0,0 +1,15 @@
+import '../App.css';
+
+function Header() {
+    return(
+        <div className='header'>
+            <div className='header_left'>
+                <img src='./avtar2.png' alt='logo' />
+            </div>
+            <div className='header_right'>
+                
+            </div>
+        </div>
+    )
+}
+export default Header;
