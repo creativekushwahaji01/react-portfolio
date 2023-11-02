0b538532863763477830ec3410f5800de495f21f
tech-hunter <kingkushwaha93@gmail.com>
2023-11-02 11:00:48 +0530

    fooyter

--- src/modules/Service.js
+++ src/modules/Service.js
@@ -11,7 +11,7 @@ const Centre = () => {
         </p>
         <div className="container reveal">
           <div className="ref2">
-            <div className="fe">
+            <div className="fe serv1 ">
               <div className="fe1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
                   <polyline points="16 18 22 12 16 6"></polyline>
@@ -23,7 +23,7 @@ const Centre = () => {
                 I create functional, beautiful, and responsive frontend applications for the web with modern technologies.
               </p>
             </div>
-            <div className="be">
+            <div className="be serv1">
               <div className="be1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
                   <path d="M18 10h-1.26A8 8 0 1 0 9 20h9a5 5 0 0 0 0-10z"></path>
@@ -34,7 +34,7 @@ const Centre = () => {
                 I build high-quality cloud-based backend services for your applications to scale and host all your users.
               </p>
             </div>
-            <div className="ux">
+            <div className="ux serv1">
               <div className="ux1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
                   <polygon points="1 6 1 22 8 18 16 22 23 18 23 2 16 6 8 2 1 6"></polygon>
@@ -47,7 +47,7 @@ const Centre = () => {
                 I create applications for real users to match their needs and expectations, satisfy their needs and ease their lives.
               </p>
             </div>
-            <div className="ui">
+            <div className="ui serv1">
               <div className="ui1">
                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="text-white">
                   <path d="M12 19l7-7 3 3-7 7-3-3z"></path>
