c4da61d791354ea093ad94deeaebc6dc8e7435a8
creativekushwahaji <creativekushwahaji01@gmail.com>
2024-05-02 17:51:09 +0530

    2 may 24

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-  import React from "react";
-  import Home from "./home";
-  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-  import Navbar from './modules/navbar';
-  import Blog from './modules/blog';
-  import Footer from "./modules/Footer";
-  
+import React from "react";
+import Home from "./home";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './modules/navbar';
+import Blog from './modules/blog';
+import Footer from "./modules/Footer";
 
-  function App() {
-    return (
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route exact path="/" element={<Home />} /> {/* Use the "index" property */}
-          <Route exact path="/blog" element={<Blog />} />
-        </Routes>
-        {/* <Home/> */}
-        <Footer/>
-      </Router>
-    );
-  }
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} /> {/* Use the "index" property */}
+        <Route path="/blog" element={<Blog />} />
+        <Route path="*" element={<Home />} /> {/* Set Home as the default route */}
+      </Routes>
+      <Footer/>
+    </Router>
+  );
+}
 
-  export default App;
+export default App;
